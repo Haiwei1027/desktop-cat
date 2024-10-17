f61fa045b7ed750ecab22d8616a9fb6c87470fdf
LEllicott02 <LauraEllicott02@outlook.com>
2024-10-17 19:01:00 +0100

    randomise position on screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-import ctypes
+import math
+import random
 import sys
 import win32api
 import win32con
@@ -20,6 +21,43 @@ def divi(vector, scale):
 
 def alwaysOnTop():    
     win32gui.SetWindowPos(pygame.display.get_wm_info()['window'], win32con.HWND_TOPMOST, 0,0,0,0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
+
+import win32con
+import win32gui
+
+def isRealWindow(hWnd):
+    '''Return True iff given window is a real Windows application window.'''
+    if not win32gui.IsWindowVisible(hWnd):
+        return False
+    if win32gui.GetParent(hWnd) != 0:
+        return False
+    hasNoOwner = win32gui.GetWindow(hWnd, win32con.GW_OWNER) == 0
+    lExStyle = win32gui.GetWindowLong(hWnd, win32con.GWL_EXSTYLE)
+    if (((lExStyle & win32con.WS_EX_TOOLWINDOW) == 0 and hasNoOwner)
+      or ((lExStyle & win32con.WS_EX_APPWINDOW != 0) and not hasNoOwner)):
+        if win32gui.GetWindowText(hWnd):
+            return True
+    return False
+
+def getWindowSizes():
+    '''
+    Return a list of tuples (handler, (width, height)) for each real window.
+    '''
+    def callback(hWnd, windows):
+        if not isRealWindow(hWnd):
+            return
+        rect = win32gui.GetWindowRect(hWnd)
+        if rect[0] < 0:
+            return
+        windows.append((hWnd, (rect[0], rect[1], rect[2] - rect[0], rect[3] - rect[1])))
+    windows = []
+    win32gui.EnumWindows(callback, windows)
+    return windows
+
+for win in getWindowSizes():
+    print(win)
+
+#input()
 
 pygame.init()
 
@@ -42,6 +80,7 @@ win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*transparent), 0, win32co
 
 
 misty_position = muli(window_size,0.5)
+misty_position = (-100,0)
 
 running = True
 while running:
@@ -53,7 +92,8 @@ while running:
     # Blit the cat image onto the screen
     screen.blit(cat_image, misty_position)
 
-    misty_position = addi(misty_position, (0,1))
+    #misty_position = addi(misty_position, (0,1))
+    misty_position = (random.randrange(0,window_size[0]),random.randrange(0,window_size[1]))
     
     # Update the display
     pygame.display.flip()
