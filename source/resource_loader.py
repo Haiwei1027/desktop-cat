15d3d193c879a006653577a29fd0558612b84cb2
Haiwei1027 <71439492+Haiwei1027@users.noreply.github.com>
2024-10-19 12:00:16 +0100

    Update resource_loader.py

--- source/resource_loader.py
+++ source/resource_loader.py
@@ -4,16 +4,12 @@ import pygame
 class ResourceLoader:
     resources = {}
     
-    def __init__(self):
-        
-        pass
-    
     def load_images(path):
         for filename in os.listdir(path):
             if filename.endswith(".png"):
                 image_path = os.path.join(path, filename)
                 image_name = os.path.splitext(filename)[0]
-                ResourceLoader.resources[image_name] = pygame.image.load(image_path)
+                ResourceLoader.resources[image_name] = pygame.image.load(image_path).convert_alpha()
                 pass
             pass
         pass
