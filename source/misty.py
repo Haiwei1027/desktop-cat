19d340e4e0759ba9891a5d7cace27cd5d54925ca
Haiwei1027 <71439492+Haiwei1027@users.noreply.github.com>
2024-10-19 14:21:31 +0100

    Restore laura's misty actions

--- source/misty.py
+++ source/misty.py
@@ -3,34 +3,42 @@ from source.entity import Entity
 from source.resource_loader import ResourceLoader
 from source.animated_sprite import AnimatedSprite
 from source.vector import *
+import random
 
 class Misty(Entity):
     
     def __init__(self, position=(0,0)):
         animated_sprite = AnimatedSprite(256,256)
         animated_sprite.animation_tracks["sat"] = ["misty_sat"]
+        animated_sprite.animation_tracks["move"] = ["misty_move"]
+        animated_sprite.animation_tracks["sleep"] = ["misty_sleep"]
         animated_sprite.animation = "sat"
         super().__init__("Misty", animated_sprite,position=position)
-        self.action = "sit"
+        self.action = "sat"
         pass
-    
-    def misty_sat():
-        cat_image = ResourceLoader.resources["i.png"].convert_alpha()
-        cat_image = pygame.transform.scale_by(cat_image, 0.4)
-        return cat_image
-
-    def misty_sleep():
-        cat_image = pygame.image.load("photos/Picture7.png").convert_alpha()
-        cat_image = pygame.transform.scale_by(cat_image, 0.05)
-        return cat_image
-
-    def misty_move():
-        cat_image = pygame.image.load("photos/h.png").convert_alpha()
-        cat_image = pygame.transform.scale_by(cat_image, 0.4)
-        return cat_image
     
     def update(self):
         super().update()
-        self.position = addi(self.position, (0,1))
+        if self.action == "sleep":
+            self.animated_sprite.animation = "sleep"
+            #make misty randomly have a sit
+            if random.randint(0, 100) == 9:
+                self.action = "sat"
+        elif self.action == "move":
+            self.animated_sprite.animation = "move"
+            self.position = addi(self.position, (random.randint(-10,10),random.randint(-10,10)))
+            #make misty randomly have a sit
+            if random.randint(0, 100) == 9:
+                self.action = "sat"
+        elif self.action == "sat":
+            self.animated_sprite.animation = "sat"
+            #make misty randomly have a move
+            if random.randint(0, 100) == 9:
+                self.action = "move"
+            #make misty randomly have a sleep
+            if random.randint(0, 100) == 9:
+                self.action = "sleep"
+        else:
+            raise Exception(f"Unknown misty action {self.action}")
         pass
     
