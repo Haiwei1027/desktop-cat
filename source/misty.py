f1fc6393a65dafdb3300fd801e071bc867532a30
Haiwei1027 <71439492+Haiwei1027@users.noreply.github.com>
2024-10-19 12:00:13 +0100

    Update misty.py

--- source/misty.py
+++ source/misty.py
@@ -1,14 +1,21 @@
 import pygame
 from source.entity import Entity
+from source.resource_loader import ResourceLoader
+from source.animated_sprite import AnimatedSprite
+from source.vector import *
 
 class Misty(Entity):
     
-    def __init__(self):
+    def __init__(self, position=(0,0)):
+        animated_sprite = AnimatedSprite(256,256)
+        animated_sprite.animation_tracks["sat"] = ["misty_sat"]
+        animated_sprite.animation = "sat"
+        super().__init__("Misty", animated_sprite,position=position)
         self.action = "sit"
         pass
     
     def misty_sat():
-        cat_image = pygame.image.load("photos/i.png").convert_alpha()
+        cat_image = ResourceLoader.resources["i.png"].convert_alpha()
         cat_image = pygame.transform.scale_by(cat_image, 0.4)
         return cat_image
 
@@ -21,3 +28,9 @@ class Misty(Entity):
         cat_image = pygame.image.load("photos/h.png").convert_alpha()
         cat_image = pygame.transform.scale_by(cat_image, 0.4)
         return cat_image
+    
+    def update(self):
+        super().update()
+        self.position = addi(self.position, (0,1))
+        pass
+    
