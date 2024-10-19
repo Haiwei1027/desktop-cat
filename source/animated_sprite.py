b86b5a0cebddf11db258e34cc82938f50fcf16ec
Haiwei1027 <71439492+Haiwei1027@users.noreply.github.com>
2024-10-19 12:00:01 +0100

    Update animated_sprite.py

--- source/animated_sprite.py
+++ source/animated_sprite.py
@@ -1,9 +1,11 @@
 from pygame.surface import Surface
-
+from source.window_manager import WindowManager
+from source.resource_loader import ResourceLoader
+import pygame
 class AnimatedSprite:
     
     def __init__(self, width, height):
-        self.surface = Surface(width, height)
+        self.surface = Surface((width, height))
         # example format {"misty_sleep":["misty_sleep1","misty_sleep2","misty_sleep3"]}
         self.animation_tracks = {}
         self.animation = ""
@@ -20,8 +22,13 @@ class AnimatedSprite:
     
     def render(self):
         # update surface with image
-        
-        # TODO get animation
-        
-        # TODO blit sprite onto surface
+        self.surface.fill(WindowManager.TRANSPARENT)
+        track = self.animation_tracks[self.animation]
+        img = ResourceLoader.resources[track[self.pointer]]
+        w_scale,h_scale = (self.surface.get_width(), self.surface.get_height())
+        w_scale /= img.get_width()
+        h_scale /= img.get_height()
+        bigger_scale = max(w_scale,h_scale)
+        img = pygame.transform.scale_by(img, bigger_scale)
+        self.surface.blit(img, (0,0))
         pass
